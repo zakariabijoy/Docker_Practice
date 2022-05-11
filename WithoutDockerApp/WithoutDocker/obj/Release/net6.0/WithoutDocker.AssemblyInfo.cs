ea902fd8206d3ce0e36e424b8cbc55352d06c1cc
zakariabijoy <zakaria.bijoy@live.com>
2022-05-11 17:17:38 +0600

    dockerize WithoutDocker dotnet web app

--- WithoutDockerApp/WithoutDocker/obj/Release/net6.0/WithoutDocker.AssemblyInfo.cs
+++ WithoutDockerApp/WithoutDocker/obj/Release/net6.0/WithoutDocker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("0147beb1-ae46-49bb-9ce7-42b9a56e3de2")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WithoutDocker")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WithoutDocker")]
